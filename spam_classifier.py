d441911a3a844222abc34d04a12e5f03961203d7
czakuou <emil.krzakuou@gmail.com>
2021-03-18 15:04:09 +0100

    open file

--- spam_classifier.py
+++ spam_classifier.py
@@ -7,5 +7,9 @@ name_dir = [next(os.walk('data/names'))[0] + '/' + filenames[i] for i in range(l
 
 # dataframe with names of files
 df = pd.read_csv(name_dir[0])
-print(df)
+# get file name
+fil = df['easy_ham_2'][0]
 
+# read file
+data = open(f'data/{df.columns[0]}/{fil}', "r")
+print(data.read())
